3a52bbb6f7fa5e946dca58346bdd634991207aff
deepShah2628 <abhip0834@gmail.com>
2021-08-25 17:39:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+
+const stringMan = (val)=>{
+var a =  document.getElementById("subject").value;
+var b =  document.getElementById("number").value;  
+const reverseWords = (sentence) =>{
+  const words = sentence.toString().split(" ")
+  const revWords = words.reverse()
+  return revWords.join(" ")
+}
+val.preventDefault();
+document.write(reverseWords(a));
+
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <div className="container">
+    <form >
+    <div className="row">
+        <div className="col-25">
+          <label htmlFor="subject">Input String</label>
+        </div>
+        <div className="col-75">
+          <textarea id="subject" name="subject"  ></textarea>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-25">
+          <label htmlFor="number">Skip Last</label>
+        </div>
+        <div className="col-75">
+          <input type="number" id="number" name="number" max="30"/>
+        </div>
+      </div>
+      <div className="row center">
+        <input type="submit" value="Run" onClick={stringMan}/>
+      </div>
+    </form>
+  </div>
+
+  )
 }
+
 
 export default App;
