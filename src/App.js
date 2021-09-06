275c36f1b71030c37b2f0a2fcf9b7f40ff93fa37
deepShah2628 <abhip0834@gmail.com>
2021-09-07 00:47:00 +0530

    updated Commit

--- src/App.js
+++ src/App.js
@@ -1,61 +1,66 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
 
+const stringMan = (val) => {
+  const paragraph = document.getElementById("subject").value;
+  const wordsNotToReverse = document.getElementById("number").value;
 
-const stringMan = (val)=>{
-var a =  document.getElementById("subject").value;
-var b =  document.getElementById("number").value;  
-function reverseWords(str) {
-  var result = '';
-  (function readWord(i = 0) {
-    var word = '';
-    if(i > str.length) {
-        return '';
-    }    
-    while(str[i] !== ' ' && i < str.length) {
-      word += str[i];
-      i++;
-    }    
-    readWord(++i); 
-    result += word + ' ';    
-  }());
-  return result.trim();
-}
+  function reverseAllButLastN(paragraph, n) {
+    return paragraph
+      .split(".")
+      .map((sentence) => {
+        const words = sentence.trim().split(" ");
 
-val.preventDefault();
-document.write(reverseWords(a));
+        const reversedWords = [];
 
-}
+        const lastReverseIndex = words.length - 1 - n;
+        for (let idx = lastReverseIndex; idx >= 0; idx--) {
+          reversedWords.push(words[idx]);
+        }
 
+        const normalWords = words.slice(words.length - n);
+        return reversedWords.concat(normalWords).join(" ");
+      })
+      .join(". ");
+  }
+  const result = reverseAllButLastN(paragraph, wordsNotToReverse);
+  document.write(
+    '<span style="font-size:18px;">Input String: ' + paragraph + "</span>"
+  );
+  document.write("<br>");
+  document.write(
+    '<span style="font-weight: bold;font-size:20px">Output String: ' +
+      result +
+      "</span>"
+  );
+};
 
 function App() {
-  return(
+  return (
     <div className="container">
-    <form >
-    <div className="row">
-        <div className="col-25">
-          <label htmlFor="subject">Input String</label>
+      <form>
+        <div className="row">
+          <div className="col-25">
+            <label htmlFor="subject">Input String</label>
+          </div>
+          <div className="col-75">
+            <textarea id="subject" name="subject"></textarea>
+          </div>
         </div>
-        <div className="col-75">
-          <textarea id="subject" name="subject"  ></textarea>
+        <div className="row">
+          <div className="col-25">
+            <label htmlFor="number">Skip Last</label>
+          </div>
+          <div className="col-75">
+            <input type="number" id="number" name="number" max="30" />
+          </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-25">
-          <label htmlFor="number">Skip Last</label>
+        <div className="row center">
+          <input type="submit" value="Run" onClick={stringMan} />
         </div>
-        <div className="col-75">
-          <input type="number" id="number" name="number" max="30"/>
-        </div>
-      </div>
-      <div className="row center">
-        <input type="submit" value="Run" onClick={stringMan}/>
-      </div>
-    </form>
-  </div>
-
-  )
+      </form>
+    </div>
+  );
 }
-
 
 export default App;
